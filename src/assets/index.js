75a96be61ce60b346ec589c4c3dacae0a4205ee4
SwastikPadasalkar197 <swastikpadasalkar3032023@gmail.com>
2026-06-07 10:44:35 +0530

    Add files via upload
    Data Binding in React App.
    Forms
    uploaded ./src files

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import COMP_LIFE from './complife.jsx';
+
+ReactDOM.render(
+    <COMP_LIFE />,
+    document.getElementById('root')
+); 
